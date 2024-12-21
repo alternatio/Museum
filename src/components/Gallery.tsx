fe42df2c24853afaecb898031260c7a5182b83c5
alternatio <shinkarev0913@mail.ru>
2024-12-21 16:02:47 +0500

    fix: gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,78 +1,76 @@
-import React, {memo, useState} from 'react'
-import style from '../styles/Gallery.module.css'
-import data from './dataOfGallery'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion } from "framer-motion";
+import React, { memo, useEffect, useState } from "react";
+import style from "../styles/Gallery.module.css";
+import data from "./dataOfGallery";
 
 const Gallery: React.FC = () => {
-  const countPictures: number = 13
-  const [arrayOfNumber, changeArrayOfNumber] = useState<number[]>([])
+  const countPictures: number = 13;
+  const [arrayOfNumber, changeArrayOfNumber] = useState<number[]>([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+  ]);
 
-  const mix: Function = (arrayOfNumber: number[]) => {
-    for(let i = 1; i <= countPictures; i++) {
-      arrayOfNumber.push(i)
+  function shuffle(array: number[]): number[] {
+    let currentIndex = array.length;
+    let result: number[] = [...array];
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [result[currentIndex], result[randomIndex]] = [
+        result[randomIndex],
+        result[currentIndex],
+      ];
     }
 
-    let swapper: number, 
-    currentIndex: number = arrayOfNumber.length, 
-    randomIndex: number
-
-    while(currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex--
-      swapper = arrayOfNumber[currentIndex]
-      arrayOfNumber[currentIndex] = arrayOfNumber[randomIndex]
-      arrayOfNumber[randomIndex] = swapper
-    }
-    return arrayOfNumber
+    return [...result];
   }
 
+  useEffect(() => {
+    changeArrayOfNumber(shuffle(arrayOfNumber));
+  }, []);
+
   return (
-    <motion.div 
-    onViewportEnter={() => changeArrayOfNumber(mix(arrayOfNumber))}
-    onViewportLeave={() => changeArrayOfNumber([])}
-    viewport={{once: true}}
-    className={style.Gallery}>
-      <div className={style.head}>
-        Галерея
-      </div>
+    <motion.div className={style.Gallery}>
+      <div className={style.head}>Галерея</div>
       <div className={style.grid}>
-        {
-          arrayOfNumber.map((value, index) => {
+        {arrayOfNumber.map((value, index) => {
+          // working in 'require'
+          // console.log(`../images/gallery${value}.jpg`)
+          // > '../images/gallery3.jpg'
 
-            // working in 'require'
-            // console.log(`../images/gallery${value}.jpg`)
-            // > '../images/gallery3.jpg'
-            
-            // not working in 'require'
-            // console.log(Object.entries(data)[value-1][1].linkPicture)
-            // > '../images/gallery3.jpg'
-            // :\
+          // not working in 'require'
+          // console.log(Object.entries(data)[value-1][1].linkPicture)
+          // > '../images/gallery3.jpg'
+          // :\
 
-            const imageLink: string = require(`../images/gallery${value}.jpg`)
-            const imageName: string = Object.entries(data)[value-1][1].namePicture
-            return (
-              <AnimatePresence>
-                <motion.div
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                exit={{opacity:0}}
-                transition={{duration: .5, type:'tween'}}
-                >
-                  <img 
+          const imageLink: string = require(`../images/gallery${value}.jpg`);
+          const imageName: string =
+            Object.entries(data)[value - 1][1].namePicture;
+          return (
+            <AnimatePresence key={index}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5, type: "tween" }}
+              >
+                <img
                   className={style.picture}
-                  src={imageLink} 
-                  alt={imageName} />
-                  <div className={style.name}>
-                    {imageName}
-                  </div>
-                </motion.div>
-              </AnimatePresence>
-            )
-          })
-        }
+                  src={imageLink}
+                  alt={imageName}
+                />
+                <div className={style.name}>{imageName}</div>
+              </motion.div>
+            </AnimatePresence>
+          );
+        })}
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default memo(Gallery)
+export default memo(Gallery);
